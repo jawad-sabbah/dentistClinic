af11240e53841050cf63701d0a77cb483ab6aaa8
Jawad sabbah <sabbah.m.jawad@gmail.com>
2025-11-22 16:38:36 +0200

    finish Login-Register -css html-

--- index.js
+++ index.js
@@ -10,11 +10,21 @@ app.set("views", "./views");
 app.use(express.static("public"));
 
 
-
 app.get('/', (req, res) => {
   res.render('main');
 }
 );
+
+app.get('/login', (req, res) => {
+  res.render('layouts/login');
+}
+); 
+
+app.get('/register', (req, res) => {
+  res.render('layouts/register');
+}
+);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
